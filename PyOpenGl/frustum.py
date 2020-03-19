9050ef69c7e5ef283f990a1df2fa44f238bd1dda
Kirill Zakharov <kirill.zakharov00@mail.ru>
2020-03-19 16:01:20 +0300

    PyOpenGL_PART2

---
+++ PyOpenGl/frustum.py
@@ -0,0 +1,59 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
+from math import cos, sin, pi
+# Процедура инициализации
+def init():
+    glEnable(GL_DEPTH_TEST)
+    glClearColor(0.5, 0.5, 0.5, 1.0) # Серый цвет для первоначальной закраски
+    gluOrtho2D(-1.0, 1.0, -1.0, 1.0) # Определяем границы рисования по горизонтали и вертикали
+
+# Процедура обработки обычных клавиш
+def keyboardkeys(key, x, y):
+    if key == b'\x1b':
+        sys.exit(0)
+    glutPostRedisplay()         # Вызываем процедуру перерисовки
+
+# Процедура рисования
+def draw(*args, **kwargs):
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # Очищаем экран и заливаем текущим цветом фона
+    glRotated(0.125,1,1,1)
+    R = 0.1
+    R1= 0.05
+    
+    glBegin(GL_TRIANGLE_FAN)
+    glColor3f(1, 0, 0)
+    glVertex3d(0, 0, 0)
+    for i in range(21):
+        glVertex3d(R*cos(i*2*pi/20), R*sin(i*2*pi/20), 0)
+    glEnd()
+
+    glBegin(GL_TRIANGLE_FAN)
+    glColor3f(0, 1, 0)
+    glVertex3d(0, 0, 1)
+    for i in range(21):
+        glVertex3d(R1*cos(i*2*pi/20), R1*sin(i*2*pi/20), 1)
+    glEnd()
+    
+    glBegin(GL_QUAD_STRIP)
+    glColor3f(0, 1, 1)
+    for i in range(21):
+        glVertex3d(R*cos(i*2*pi/20), R*sin(i*2*pi/20), 0)
+        glVertex3d(R1*cos((i+1)*2*pi/20), R1*sin((i+1)*2*pi/20), 1)
+    glEnd()
+
+    glutSwapBuffers()           # Меняем буферы
+    glutPostRedisplay()         # Вызываем процедуру перерисовки
+
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+glutInitWindowSize(800, 600)
+glutInitWindowPosition(50, 50)
+glutInit(sys.argv)
+glutCreateWindow(b"OpenGL")
+# Определяем процедуру, отвечающую за рисование
+glutDisplayFunc(draw)
+# Определяем процедуру, отвечающую за обработку обычных клавиш
+glutKeyboardFunc(keyboardkeys)
+# Вызываем нашу функцию инициализации
+init()
+glutMainLoop()
